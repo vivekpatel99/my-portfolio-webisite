25eadd0034d29593aa718f489100b98c68d38109
vivekpatel99 <vivpatel99@gmail.com>
2026-09-07 09:44:48 +0200

    Revert "Merge pull request #38 from vivekpatel99/codex/gate-3-project-fit-diagnostic"
    This reverts commit cd35e406c97d41670de1c3d8ace043a81b6852f5, reversing
    changes made to 5f98a5eb16d0d1480d5169dc2aa37c8e92d4ca15.

--- tests/qa/qa.config.js
+++ tests/qa/qa.config.js
@@ -39,12 +39,7 @@ const passiveProjects = [
     name: 'prod-mobile',
     use: { ...devices['iPhone 14'], browserName: 'chromium', baseURL: prodURL },
   },
-].map((project) => ({
-  ...project,
-  testMatch: project.name.startsWith('preview-')
-    ? [...passiveSpecs, 'qa-project-fit.spec.js']
-    : passiveSpecs,
-}));
+].map((project) => ({ ...project, testMatch: passiveSpecs }));
 
 const liveProjects = includeLiveContactSubmit
   ? [
